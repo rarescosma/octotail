6f3bac805c5da60ea22347059196ae3aae703913
Rareș Cosma <rares@getbetter.ro>
2024-10-11 15:48:03 +0300

    setup-py: auto install requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
+from codecs import open
+from os import path
+
 from setuptools import find_packages, setup
+
+dot = path.abspath(path.dirname(__file__))
+
+# get the dependencies and installs
+with open(path.join(dot, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in all_reqs if x.startswith("git+")]
 
 setup(
     name="octotail",
@@ -16,4 +28,6 @@ setup(
     keywords="github-actions, tail, post-receive, codecrafters, git",
     packages=find_packages(exclude=["docs", "tests*"]),
     include_package_data=True,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
 )
