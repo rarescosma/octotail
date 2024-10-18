7688074e04b3da14984426a25529be8b303cef87
Rareș Cosma <rares@getbetter.ro>
2024-10-18 18:45:07 +0300

    extract whan we can from mitm using returns (#3)

--- octotail/mitm.py
+++ octotail/mitm.py
@@ -11,10 +11,15 @@ from dataclasses import dataclass
 from pathlib import Path
 from queue import Empty
 from threading import Event
+from typing import cast
 
 from mitmproxy.tools.main import mitmdump
 from pykka import ActorRef, ThreadingActor
-from returns.result import Result, Success
+from returns.converters import result_to_maybe
+from returns.maybe import Maybe, Nothing, Some
+from returns.pipeline import flow
+from returns.pointfree import map_
+from returns.result import Result, Success, safe
 from xdg.BaseDirectory import xdg_data_home
 
 from octotail.utils import Retry, debug, is_port_open, retries
@@ -78,7 +83,7 @@ class ProxyWatcher(ThreadingActor):
                 if line:
                     buffer.append(line)
                 elif old_line == "" and buffer:
-                    self._process_buffer("".join(buffer), "".join(old_buffer))
+                    _extract_ws_sub("".join(buffer), "".join(old_buffer)).apply(Some(self.mgr.tell))
                     old_buffer = copy.deepcopy(buffer)
                     buffer = []
                 else:
@@ -86,32 +91,44 @@ class ProxyWatcher(ThreadingActor):
                 old_line = line
         debug("exiting")
 
-    def _process_buffer(self, buffer: str, old_buffer: str) -> None:
-        if (
-            MARKERS.ws_header in old_buffer
-            and MARKERS.ws_host in old_buffer
-            and MARKERS.ws_action in buffer
-        ):
-            url = old_buffer[old_buffer.index(MARKERS.ws_host) :]
-            if (job_id := _extract_job_id(buffer)) is not None:
-                self.mgr.tell(WsSub(url=url, subs=buffer, job_id=job_id))
+
+def _extract_ws_sub(buffer: str, old_buffer: str) -> Maybe[WsSub]:
+    if not (
+        MARKERS.ws_header in old_buffer
+        and MARKERS.ws_host in old_buffer
+        and MARKERS.ws_action in buffer
+    ):
+        return Nothing
+
+    return flow(
+        _extract_job_id(buffer),
+        map_(
+            lambda job_id: WsSub(
+                url=old_buffer[old_buffer.index(MARKERS.ws_host) :],
+                subs=buffer,
+                job_id=cast(int, job_id),
+            )
+        ),
+        result_to_maybe,
+    )
 
 
-def _extract_job_id(buffer: str) -> int | None:
-    with suppress(Exception):
-        dec = (base64.b64decode(k) for k in json.loads(buffer)["subscribe"])
-        items = (str(json.loads(d[: d.index(b"}") + 1].decode())["c"]) for d in dec)
-        good = next(item for item in items if item.startswith("check_runs"))
-        return int(good.split(":")[1])
-    return None
+@safe
+def _extract_job_id(buffer: str) -> int:
+    dec = (base64.b64decode(k) for k in json.loads(buffer)["subscribe"])
+    items = (str(json.loads(d[: d.index(b"}") + 1].decode())["c"]) for d in dec)
+    good = next(item for item in items if item.startswith("check_runs"))
+    return int(good.split(":")[1])
 
 
-def _mitmdump_wrapper(q: multiprocessing.Queue, port: int) -> None:
+def _mitmdump_wrapper(queue: multiprocessing.Queue, port: int) -> None:
     sys.argv = (
         f"mitmdump --flow-detail=4 --no-rawtcp -p {port} --set confdir={MITM_CONFIG_DIR}".split()
     )
+    # hijack .isatty and always return False to disable colors & shenanigans
     setattr(sys.stdout, "isatty", lambda: False)
-    setattr(sys.stdout, "write", q.put)
+    # hijack .write so lines go to our queue instead
+    setattr(sys.stdout, "write", queue.put)
     mitmdump()
 
 
